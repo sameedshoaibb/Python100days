2de09b4beeee137a70a5c1a8d905c04623665fd2
abrehman <waseem@example.com>
2019-09-28 12:24:36 +0500

    Day One code

---
+++ day_one.py
@@ -0,0 +1,13 @@
+#!/usr/bin python
+
+#1- to correspond list in list
+
+# names=["Peter Parker","Wilson"]
+# heroes=["Spider Man","BatMan"]
+# universe=["Marvel","Zoom"]
+
+# for index,name in enumerate(names):
+#     hero=heroes[index]
+#     univers=universe[index]
+#     print "{} is the Hero of {}. The production company is {}".format(name,hero,univers)  
+
