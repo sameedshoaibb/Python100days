3e0c0dcb99256be28359c3519e940500f49c9a55
sameedshoaibb <sameedshoaib@live.co.uk>
2019-11-15 20:48:32 +0500

    Update

--- day_four.py
+++ day_four.py
@@ -2,14 +2,21 @@
 
 """ start """
 
+array = []
+
+def INPUT(value):
+    print("Yalla!"+str(value))
+    for i in range(0, value):
+        TAKE_INPUT2 = eval(input("Enter the number"))
+        array.append(TAKE_INPUT2)
+    calculate(array)
+
+
+def calculate(val):
+    sum1 = 0
+    for d in val:
+        sum1 += d + sum1 
+    print(sum1/TAKE_INPUT)
 
 TAKE_INPUT = eval(input("Enter the number of elements to be inserted:"))
-array = []
-for i in range(0, TAKE_INPUT):
-    TAKE_INPUT2 = eval(input("Enter the number"))
-    array.append(TAKE_INPUT2)
-
-def sumer(val):
-    print("SAMEED"+ val)
-
-print(sumer(array)/len(array))
+print(INPUT(TAKE_INPUT))
