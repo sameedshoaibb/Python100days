00b8fa9e48543456ef134d7c29fe571eecf10809
sameedshoaibb <sameedshoaib@Live.co.uk>
2020-09-03 16:35:08 +0500

    Adding microservices

--- version_cleaner.py
+++ version_cleaner.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-''' Pheli line'''
+''' Service that will give running Testing machine's '''
 
 import boto3
 REGION = 'eu-central-1'
+ENV = "stage"
 
 def get_name():
     ''' Get data '''
@@ -12,7 +13,7 @@ def get_name():
                                                         'Values': ['running']}])
     for instance in running_instances:
         for tag in instance.tags:
-            print("MR REHMAN",tag)
+            # print("MR REHMAN",tag)
             if tag['Key'] == 'Name':
                 name = tag['Value']
                 empty_list.append(name)
@@ -23,31 +24,88 @@ def data_simplify(sname):
     shortlist_machines = []
     list_of_name = sname
     for i in list_of_name:
-        if 'test' in i:
+        if ENV in i:
             shortlist_machines.append(i)
+    print(shortlist_machines)
     return shortlist_machines
 
 def version_split(data):
-    ''' Version split '''
+    ''' Version split from service name '''
     unique = []
     for i in data:
         data = i.split('-v')
-        sa =  data[0]+ " "+ "v"+ data[1]
-        unique.append(sa)
+        service =  data[0]+ " "+ "v"+ data[1]
+        unique.append(service)
     return unique
 
 def unique(d):
+    ''' Get only unique service '''
     list_set = set(d) 
     unique_list = (list(list_set))
     for x in unique_list:
-        print(x)
+        one = x.split(" ")
+        if "iauth" in one[0]:
+            print("platform-auth",one[1])
+            
+        elif "ivamproxy" in one[0]:
+            print("vam-proxy",one[1])
+
+        elif "gateway" in one[0]:
+            print("platform-api-gateway",one[1])
+
+        elif "vam" in one[0]:
+            print("vam",one[1])
+
+        elif "admin" in one[0]:
+            print("platform-auth-admin",one[1])
+
+        elif "ica" in one[0]:
+            print("network-connection-logs",one[1])
+
+        elif "queue-stage-api" in one[0]:
+            print("platform-queue",one[1])
+        
+        elif "queue-stage-cron" in one[0]:
+            print("platform-queue",one[1])
+
+        elif "ib" in one[0]:
+            print("platform-billing-api",one[1])
+
+        elif "iwaf" in one[0]:
+            print("platform-waf",one[1])
+
+        elif "ianalytics" in one[0]:
+            print("atom-bi-api",one[1])
+
+        elif "rabbitmq" in one[0]:
+            print("event-broadcast-manager",one[1])
+
+        elif "test-cron" in one[0]:
+            print("event-broadcast-cron",one[1])
+            
+        elif "billing" in one[0]:
+            print("platform-billing",one[1])
+
+        elif "ist" in one[0]:
+            print("platform-speed-test",one[1])
+
+        elif "idxn" in one[0]:
+            print("dxn-api",one[1])
+
+        elif "secure" in one[0]:
+            print("secure-website",one[1])
+
+        elif "ivap" in one[0]:
+            print("vpn-account-profiling",one[1])
+        else:
+            print("Try again",one)
 
 def main():
     ''' Main Fucntion '''
     name = get_name()
-    data_simplify1 = data_simplify(name)
-    d = version_split(data_simplify1)
-    unique(d)
+    shortlist_machines = data_simplify(name)
+    splitted_version = version_split(shortlist_machines)
+    unique(splitted_version)
 
 if __name__ == "__main__":
     main()
