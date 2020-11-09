6cb117a98d682f324e74bcbc95a7f1acccfa86fd
sameedshoaibb <sameedshoaib@Live.co.uk>
2020-11-09 22:12:00 +0500

    Adding feature in script

--- python-selenium-basic/ex02.py
+++ python-selenium-basic/ex02.py
@@ -1,23 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
-options = webdriver.ChromeOptions()
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option('useAutomationExtension', False)
-options.add_argument("--disable-blink-features=AutomationControlled")
-driver = webdriver.Chrome(executable_path="./drivers/chromedriver")
-driver.get('https://hypeauditor.com/login/?r=app/redirect')
-#browser = webdriver.Chrome(executable_path="./drivers/chromedriver")
-#browser.get('https://hypeauditor.com/preview/shaunylbenson')
+browser = webdriver.Chrome(executable_path="./drivers/chromedriver")
+browser.get('https://hypeauditor.com/login/?r=app/redirect')
 
 
-username = driver.find_element_by_id("email")
-password = driver.find_element_by_id("password")
+username = browser.find_element_by_id("email")
+password = browser.find_element_by_id("password")
 
-username.send_keys("YourUsername")
-password.send_keys("Pa55worD")
+username.send_keys("boxetiv149@appnox.com")
+password.send_keys("boxetiv149@appnox.com")
 
-driver.find_element_by_class("login-form-state--link").click()
+browser.find_element_by_class_name('button-block').click()
+list_of_influencers = ['linaderman','piperoninsta','natia_natsi','yaseminxkaradag','tesdress','hijab_by_turkey','muslimawear']
+for i in list_of_influencers:
+    browser.get('https://app.hypeauditor.com/preview/{}'.format(i))
+# browser.get('https://app.hypeauditor.com/preview/natia_natsi')
 
-sleep(1115)
-#browser.quit()
+    html_source = browser.page_source
+    print (i)
+    with open('dog_breeds_reversed.txt', 'w') as writer:
+        writer.write(html_source)
+        
+    with open('dog_breeds_reversed.txt', 'r') as reader:
+        line = reader.readline()
+        while line != '':  # The EOF char is an empty string
+            line = reader.readline()
+            if "kyb-user-info-v2__sub-title" in line:
+                a = line.split(">")[1]
+                b = a.split("<")[0]
+                print(b)
